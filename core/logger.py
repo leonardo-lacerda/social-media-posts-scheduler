a7d08be95fdb0aac0dd3e7059ceacdef2b849abe
ClimenteA <climente.alin@gmail.com>
2025-05-10 16:14:16 +0300

    x-cloak on description, text decoration none on schedule navigation

--- core/logger.py
+++ core/logger.py
@@ -1,10 +1,6 @@
 import requests
-from django.contrib import messages
-from django.shortcuts import redirect
-from loguru import logger as log
-from functools import wraps
-from social_django.models import UserSocialAuth
 from .settings import logpath
+from loguru import logger as log
 from .settings import NOTIFICATION_API_KEY, NOTIFICATION_API_URL
 
 
@@ -35,37 +31,3 @@ def send_notification(email: str, message: str):
         response.raise_for_status()
     except Exception as err:
         log.exception(err)
-
-
-# def log_exception(view_func):
-#     @wraps(view_func)
-#     def wrapper(request, *args, **kwargs):
-#         try:
-#             return view_func(request, *args, **kwargs)
-#         except Exception as err:
-#             account_id = "N/A"
-#             if hasattr(request, "user"):
-#                 try:
-#                     user_social_auth = UserSocialAuth.objects.filter(
-#                         user=request.user
-#                     ).first()
-#                     account_id = user_social_auth.uid
-#                 except Exception as serr:
-#                     log.error(f"User not logged in. {serr}")
-
-#             log.error(f"Account ID: {account_id} got error: {err}")
-#             log.exception(err)
-#             send_notification(
-#                 email="ImPosting",
-#                 message=f"AccountId: {account_id} got error {err}",
-#             )
-#             messages.add_message(
-#                 request,
-#                 messages.ERROR,
-#                 "",
-#                 extra_tags="🟥 Error!",
-#             )
-#             log.debug(f"THE REQUEST PATH: {request.path}")
-#             return redirect("/integrations/")
-
-#     return wrapper
